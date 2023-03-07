d7624cca36334c0e9b412f5fce603e94d7f35af5
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-03-07 19:08:47 +0100

    isAuthenticated test à continuer

--- models/Favourites.js
+++ models/Favourites.js
@@ -4,6 +4,7 @@ const Favourite = mongoose.model("Favourite", {
   name: String,
   image: String,
   token: String,
+  //   user: String,
 });
 
 module.exports = Favourite;
--- routes/review.js
+++ routes/review.js
@@ -3,52 +3,59 @@ const router = express.Router();
 
 const Review = require("../models/Review");
 
+// Import du middleware isAuthenticated
+const isAuthenticated = require("../middlewares/isAuthenticated");
+
 //route 1 pour ajouter des review en post++++++++++++++++++++++++++
 
-router.post("/addreview", async (req, res) => {
-  // router.post("/addreview/:name, async (req, res) => {
-  //si l'id ne fonctionne pas, il va falloir passer par le nom du jeu comme les favoris
+router.post(
+  "/addreview",
+  // isAuthenticated,
+  async (req, res) => {
+    // router.post("/addreview/:name, async (req, res) => {
+    //si l'id ne fonctionne pas, il va falloir passer par le nom du jeu comme les favoris
 
-  try {
-    const { title, description, token, name, count } = req.body;
+    try {
+      const { title, description, token, name, count } = req.body;
 
-    //déclaration nouvel review
-    const newReview = new Review({
-      title: title,
-      description: description,
-      token: token,
-      name: name,
-      count: count,
-    });
+      //déclaration nouvel review
+      const newReview = new Review({
+        title: title,
+        description: description,
+        token: token,
+        name: name,
+        count: count,
+      });
 
-    //il faudra vérifier que un user ne peut poster qu'une seule review par jeu
-    const newReviewAlreadyAdded = await Review.findOne({ name, token });
+      //il faudra vérifier que un user ne peut poster qu'une seule review par jeu
+      const newReviewAlreadyAdded = await Review.findOne({ name, token });
 
-    if (newReviewAlreadyAdded) {
-      return res
-        .status(409)
-        .json({ message: "you have already added a review for this game" });
+      if (newReviewAlreadyAdded) {
+        return res
+          .status(409)
+          .json({ message: "you have already added a review for this game" });
+      }
+      // pour initier le compteur à 0
+      if (!count) {
+        newReview.count = 0;
+      }
+
+      await newReview.save();
+
+      const clientResponse = {
+        title: newReview.title,
+        description: newReview.description,
+        id: newReview.id,
+        name: newReview.name,
+        count: newReview.count,
+      };
+      res.status(200).json(clientResponse);
+    } catch (error) {
+      console.log(error.message);
+      res.status(400).json({ message: error.message });
     }
-    // pour initier le compteur à 0
-    if (!count) {
-      newReview.count = 0;
-    }
-
-    await newReview.save();
-
-    const clientResponse = {
-      title: newReview.title,
-      description: newReview.description,
-      id: newReview.id,
-      name: newReview.name,
-      count: newReview.count,
-    };
-    res.status(200).json(clientResponse);
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({ message: error.message });
   }
-});
+);
 
 //route 2 pour lire les reviews+++++++++++++++++++++++++++++++++++
 
